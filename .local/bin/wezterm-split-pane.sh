3c20313b56d0f227342c83c83e5c9d34c87d757b
Quan Tong <quantonganh@gmail.com>
2023-08-19 11:26:16 +0700

    merge all scripts into one

--- .local/bin/wezterm-split-pane.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-pane_id=$(wezterm cli get-pane-direction down)
-if [ -z "${pane_id}" ]; then
-  pane_id=$(wezterm cli split-pane)
-fi
-
-wezterm cli activate-pane-direction --pane-id $pane_id down

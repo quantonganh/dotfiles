b93ae6b16f0856f64ec2094f1c002462df78378d
Quan Tong <quantonganh@gmail.com>
2024-06-27 12:45:13 +0700

    format wezterm output as json to parse program name correctly

--- .local/bin/hx-open.sh
+++ .local/bin/hx-open.sh
@@ -7,8 +7,9 @@ if [ -z "${pane_id}" ]; then
   pane_id=$(wezterm cli split-pane --right --percent 80)
 fi
 
-program=$(wezterm cli list | awk -v pane_id="$pane_id" '$3==pane_id { print $6 }')
-if [ "$program" = "hx" ]; then
+program=$(wezterm cli list --format json | jq --arg pane_id $pane_id -r '.[] | select(.pane_id  == ($pane_id | tonumber)) | .title' | awk '{ print $1 }')
+program_name=$(basename $program)
+if [ "$program_name" = "hx" ]; then
   echo ":open ${fpath}\r" | wezterm cli send-text --pane-id $pane_id --no-paste
 else
   echo "hx ${fpath}" | wezterm cli send-text --pane-id $pane_id --no-paste
